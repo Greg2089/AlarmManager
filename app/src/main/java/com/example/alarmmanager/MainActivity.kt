82127b1f21e43f5442b344cf6d7dc50e0af7a472
Grigoriy Lyalko <98054846+Greg2089@users.noreply.github.com>
2023-10-03 11:32:33 +0300

    1) Настройка

--- app/src/main/java/com/example/alarmmanager/MainActivity.kt
+++ app/src/main/java/com/example/alarmmanager/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.alarmmanager
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.alarmmanager.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
